ea72b18f26c8086348af18fce21d6493f46a86e1
hoanhtranh90 <nguyenkhacsang100@gmail.com>
2020-11-09 11:16:58 +0700

    sudo

---
+++ src/main/java/com/github/messenger4j/quickstart/boot/WebhookController.java
@@ -0,0 +1,155 @@
+package com.github.messenger4j.quickstart.boot;
+
+import static com.github.messenger4j.Messenger.CHALLENGE_REQUEST_PARAM_NAME;
+import static com.github.messenger4j.Messenger.MODE_REQUEST_PARAM_NAME;
+import static com.github.messenger4j.Messenger.SIGNATURE_HEADER_NAME;
+import static com.github.messenger4j.Messenger.VERIFY_TOKEN_REQUEST_PARAM_NAME;
+import static com.github.messenger4j.send.message.richmedia.RichMediaAsset.Type.AUDIO;
+import static com.github.messenger4j.send.message.richmedia.RichMediaAsset.Type.FILE;
+import static com.github.messenger4j.send.message.richmedia.RichMediaAsset.Type.IMAGE;
+import static com.github.messenger4j.send.message.richmedia.RichMediaAsset.Type.VIDEO;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+import com.github.messenger4j.Messenger;
+import com.github.messenger4j.common.WebviewHeightRatio;
+import com.github.messenger4j.exception.MessengerApiException;
+import com.github.messenger4j.exception.MessengerIOException;
+import com.github.messenger4j.exception.MessengerVerificationException;
+import com.github.messenger4j.send.MessagePayload;
+import com.github.messenger4j.send.MessagingType;
+import com.github.messenger4j.send.NotificationType;
+import com.github.messenger4j.send.SenderActionPayload;
+import com.github.messenger4j.send.message.RichMediaMessage;
+import com.github.messenger4j.send.message.TemplateMessage;
+import com.github.messenger4j.send.message.TextMessage;
+import com.github.messenger4j.send.message.quickreply.LocationQuickReply;
+import com.github.messenger4j.send.message.quickreply.QuickReply;
+import com.github.messenger4j.send.message.quickreply.TextQuickReply;
+import com.github.messenger4j.send.message.richmedia.UrlRichMediaAsset;
+import com.github.messenger4j.send.message.template.ButtonTemplate;
+import com.github.messenger4j.send.message.template.GenericTemplate;
+import com.github.messenger4j.send.message.template.ListTemplate;
+import com.github.messenger4j.send.message.template.ReceiptTemplate;
+import com.github.messenger4j.send.message.template.button.Button;
+import com.github.messenger4j.send.message.template.button.CallButton;
+import com.github.messenger4j.send.message.template.button.LogInButton;
+import com.github.messenger4j.send.message.template.button.LogOutButton;
+import com.github.messenger4j.send.message.template.button.PostbackButton;
+import com.github.messenger4j.send.message.template.button.UrlButton;
+import com.github.messenger4j.send.message.template.common.Element;
+import com.github.messenger4j.send.message.template.receipt.Address;
+import com.github.messenger4j.send.message.template.receipt.Adjustment;
+import com.github.messenger4j.send.message.template.receipt.Item;
+import com.github.messenger4j.send.message.template.receipt.Summary;
+import com.github.messenger4j.send.recipient.IdRecipient;
+import com.github.messenger4j.send.senderaction.SenderAction;
+import com.github.messenger4j.userprofile.UserProfile;
+import com.github.messenger4j.webhook.Event;
+import com.github.messenger4j.webhook.event.AccountLinkingEvent;
+import com.github.messenger4j.webhook.event.AttachmentMessageEvent;
+import com.github.messenger4j.webhook.event.MessageDeliveredEvent;
+import com.github.messenger4j.webhook.event.MessageEchoEvent;
+import com.github.messenger4j.webhook.event.MessageReadEvent;
+import com.github.messenger4j.webhook.event.OptInEvent;
+import com.github.messenger4j.webhook.event.PostbackEvent;
+import com.github.messenger4j.webhook.event.QuickReplyMessageEvent;
+import com.github.messenger4j.webhook.event.TextMessageEvent;
+import com.github.messenger4j.webhook.event.attachment.Attachment;
+import com.github.messenger4j.webhook.event.attachment.LocationAttachment;
+import com.github.messenger4j.webhook.event.attachment.RichMediaAttachment;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * This is the main class for inbound and outbound communication with the Facebook Messenger Platform. The callback
+ * handler is responsible for the webhook verification and processing of the inbound messages and events. It showcases
+ * the features of the Messenger Platform.
+ *
+ * @author Max Grabenhorst
+ */
+@RestController
+@RequestMapping("/callback")
+public class WebhookController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
+
+    private final Messenger messenger;
+
+    @Autowired
+    public WebhookController(final Messenger messenger) {
+        this.messenger = messenger;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
+                                                @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken, @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
+        logger.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode, verifyToken, challenge);
+        try {
+            this.messenger.verifyWebhook(mode, verifyToken);
+            return ResponseEntity.ok(challenge);
+        } catch (MessengerVerificationException e) {
+            logger.warn("Webhook verification failed: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> handleCallback(@RequestBody final String payload, @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) throws MessengerVerificationException {
+        this.messenger.onReceiveEvents(payload, of(signature), event -> {
+            if (event.isTextMessageEvent()) {
+                try {
+                    logger.info("0");
+                    handleTextMessageEvent(event.asTextMessageEvent());
+                    logger.info("1");
+                } catch (MessengerApiException e) {
+                    logger.info("2");
+                    e.printStackTrace();
+                } catch (MessengerIOException e) {
+                    logger.info("3");
+                    e.printStackTrace();
+                }
+            } else {
+                String senderId = event.senderId();
+                sendTextMessageUser(senderId, "Tôi là bot chỉ có thể xử lý tin nhắn văn bản.");
+            }
+        });
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    private void handleTextMessageEvent(TextMessageEvent event) throws MessengerApiException, MessengerIOException {
+        final String senderId = event.senderId();
+        sendTextMessageUser(senderId, "Xin chào! Đây là chatbot được tạo từ ứng dụng Spring Boot");
+
+    }
+
+    private void sendTextMessageUser(String idSender, String text) {
+        try {
+            final IdRecipient recipient = IdRecipient.create(idSender);
+            final NotificationType notificationType = NotificationType.REGULAR;
+            final String metadata = "DEVELOPER_DEFINED_METADATA";
+
+            final TextMessage textMessage = TextMessage.create(text, empty(), of(metadata));
+            final MessagePayload messagePayload = MessagePayload.create(recipient, MessagingType.RESPONSE, textMessage,
+                    of(notificationType), empty());
+            this.messenger.send(messagePayload);
+        } catch (MessengerApiException | MessengerIOException e) {
+            handleSendException(e);
+        }
+    }
+
+    private void handleSendException(Exception e) {
+        logger.error("Message could not be sent. An unexpected error occurred.", e);
+    }
+}

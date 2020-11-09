65289a6db70013052b466be57851566402b9dff3
hoanhtranh90 <nguyenkhacsang100@gmail.com>
2020-11-09 13:57:28 +0700

    success

--- src/main/java/com/github/messenger4j/quickstart/boot/WebhookController.java
+++ src/main/java/com/github/messenger4j/quickstart/boot/WebhookController.java
@@ -141,7 +141,7 @@ public class WebhookController {
         String[] arr = event.text().split(" ");
         System.out.println(arr[0]);
         System.out.println(arr[1]);
-        String url = "https://https://tse-sus.herokuapp.com/api/tu-cam/1";
+        String url = "https://tse-sus.herokuapp.com/api/tu-cam/1";
         Document homePage = Jsoup.connect(url)
 //                .method(Connection.Method.POST)
 //                .data("user", arr[0])
